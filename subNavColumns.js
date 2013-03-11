a3b584d53cc73b17daea49338cf938e76135d701
Ken Seals <kenseals@gmail.com>
2013-03-11 13:24:02 -0500

    shortened var definitions

--- subNavColumns.js
+++ subNavColumns.js
@@ -15,15 +15,15 @@
 		return this.each(function() {        
 
 			// Define variables
-			var $this = $(this);
-			var $subNav = $this.find('li > ul');
-			var $subNavParent = $subNav.parent('li');
-	    	var $l = $subNav.children('li').length;
-	    	var $mL = settings.minLength;
-	    	var $L = Math.round($l / $mL);
-	    	var $x = $this.children('li').length;
-	    	var $n = Math.round($x / 2);
-	    	var $y = $subNavParent.index();
+			var $this = $(this),
+				$subNav = $this.find('li > ul'),
+				$subNavParent = $subNav.parent('li'),
+				$l = $subNav.children('li').length,
+				$mL = settings.minLength,
+				$L = Math.round($l / $mL),
+				$x = $this.children('li').length,
+				$n = Math.round($x / 2),
+				$y = $subNavParent.index();
 
 	    	// If subnav is longer than y, add .nav-columns class
 			if ($l > $mL) {
